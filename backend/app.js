336c64a63942a86bf18abc276a60cd39216d9604
josepieru <josepieru@hotmail.com>
2024-11-17 16:44:52 -0500

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,19 @@
-//The purpose of this file is to handle any request 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const usersRoutes = require('./routes/users-routes');
 const employeeRoutes = require('./routes/employee-routes');
+const paystubRoutes = require('./routes/paystub-route'); 
 const HttpError = require('./models/http-error');
 
 const app = express();
-app.use(bodyParser.json()); 
-
 
 app.use(bodyParser.json());
 
-
 app.use('/api/users', usersRoutes);
 app.use('/api/employees', employeeRoutes);
+app.use('/api/paystub', paystubRoutes); 
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
@@ -31,12 +29,14 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-.connect('mongodb+srv://simplepaycheck:7m8rrwWexhLUSCA4@cluster0.zj08c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => {
-  app.listen(8080);
-})
-.catch(err => {
-  console.log(err);
-});
-
-
+  .connect('mongodb+srv://simplepaycheck:7m8rrwWexhLUSCA4@cluster0.zj08c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  
+  })
+  .then(() => {
+    app.listen(8080, () => {
+      console.log('Connected to the database and listening on port 8080.');
+    });
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB:', err);
+  });
