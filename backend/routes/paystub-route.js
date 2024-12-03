71765fc0992337fc3e0b6bd15caa09822f197d0b
josepieru <josepieru@hotmail.com>
2024-12-02 22:59:19 -0500

    Update paystub-route.js

--- backend/routes/paystub-route.js
+++ backend/routes/paystub-route.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { generatePaystub, getAllPaystubs } = require('../controllers/paystubController');
-
+const { generatePaystub, getAllPaystubs, getPaystubsByEmployeeId } = require('../controllers/paystubController');
+const paystubController = require('../controllers/paystubController');
 const router = express.Router();
 
 router.post('/', generatePaystub);
 router.get('/', getAllPaystubs);  
+router.get('/employee/:id', getPaystubsByEmployeeId);
+router.get('/paystub/employee/:id', paystubController.getPaystubsByEmployeeId);
 
 module.exports = router;
+
