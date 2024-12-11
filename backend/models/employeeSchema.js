3c9228a89db5e4ba5e99ba035403d3efa6721fa0
josepieru <josepieru@hotmail.com>
2024-12-11 18:40:45 -0500

    Add files via upload

--- backend/models/employeeSchema.js
+++ backend/models/employeeSchema.js
@@ -14,4 +14,4 @@ const employeeSchema = new Schema({
     taxes: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('employee', employeeSchema);
+module.exports = mongoose.model('Employee', employeeSchema);
